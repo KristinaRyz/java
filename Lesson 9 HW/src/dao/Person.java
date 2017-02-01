8d244a098615c988505654b9fe2c705e192304b9
KristinaRyz <www>
2017-02-01 13:18:14 +0200

    01.02.17

---
+++ Lesson 9 HW/src/dao/Person.java
@@ -0,0 +1,48 @@
+package dao;
+
+public class Person {
+
+		private String name;
+		private int age;
+		private boolean married;
+		
+		public Person(String name, int age, boolean married) {
+			super();
+			this.name = name;
+			this.age = age;
+			this.married = married;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public boolean isMarried() {
+			return married;
+		}
+
+		public void setMarried(boolean married) {
+			this.married = married;
+		}
+
+		@Override
+		public String toString() {
+			return "Person [name = " + name + ", age = " + age + ", married = " + married + "]";
+		}
+		
+		
+
+	}
+
